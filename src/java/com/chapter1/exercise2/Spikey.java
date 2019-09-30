5270cfef30ed60693d448500ded45eddd921046d
Mita Rakotoma <mita.rakotoma@gmail.com>
2019-09-29 19:03:16 -0500

    chpt 1 ex1-8

---
+++ src/java/com/chapter1/exercise2/Spikey.java
@@ -0,0 +1,17 @@
+package com.chapter1.exercise2;
+
+public class Spikey {
+    public static void main (String[] args){
+        tree();
+    }
+
+    public static void tree (){
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+
+    }
+}
--- src/java/com/chapter1/exercise7/Mantra.java
+++ src/java/com/chapter1/exercise7/Mantra.java
@@ -1,8 +1,15 @@
-package java.com.chapter1.exercise7;
+package com.chapter1.exercise7;
 
 public class Mantra {
     public static void main(String[] args){
+        method1();
+        method1();
+    }
 
+    public static void method1() {
+        System.out.println("There's one thing every coder must understand:");
+        System.out.println("The System.out.println command.");
+        System.out.println();
     }
 
 
