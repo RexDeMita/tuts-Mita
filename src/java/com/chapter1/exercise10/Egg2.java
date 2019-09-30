6b944a596155c552c20c9e06307f9b5cf4a19cba
Mita Rakotoma <mita.rakotoma@gmail.com>
2019-09-30 14:03:58 -0500

    chpt 1 exercises 9-10, done with chpt 1

---
+++ src/java/com/chapter1/exercise10/Egg2.java
@@ -0,0 +1,35 @@
+package com.chapter1.exercise10;
+
+public class Egg2 {
+    public static void main (String[] args){
+        //Generate the egg output
+        //eliminate redundancy
+        topHalf();
+        bottomHalf();
+        middle();
+        System.out.print("");
+        topHalf();
+        bottomHalf();
+        middle();
+        bottomHalf();
+        topHalf();
+        middle();
+        bottomHalf();
+    }
+
+    public static void topHalf (){
+        System.out.println("  _______  ");
+        System.out.println(" /       \\ ");
+        System.out.println("/         \\");
+
+    }
+
+    public static void bottomHalf (){
+        System.out.println("\\         / ");
+        System.out.println(" \\_______/  ");
+    }
+
+    public static void middle(){
+        System.out.println("-\"-'-\"-'-\"-");
+    }
+}
