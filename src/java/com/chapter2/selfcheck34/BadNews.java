528be3ba5c078916e5d18c1ad9dfde08f5c6bec7
Mita Rakotoma <mita.rakotoma@gmail.com>
2019-10-01 16:17:02 -0500

    chpt 2 self check 23 - 39, done with self check

---
+++ src/java/com/chapter2/selfcheck34/BadNews.java
@@ -0,0 +1,68 @@
+package com.chapter2.selfcheck34;
+
+public class BadNews {
+    //Fix error in  the program below
+
+    //public static final int MAX_ODD = 21;
+    //
+    //    public static void writeOdds() {
+    //        // print each odd number
+    //        for (int count = 1; count <= (MAX_ODD - 2); count++) {
+    //            System.out.print(count + " ");
+    //            count = count + 2;
+    //        }
+    //
+    //        // print the last odd number
+    //        System.out.print(count + 2);
+    //    }
+    //
+    //    public static void main(String[] args) {
+    //        // write all odds up to 21
+    //        writeOdds();
+    //
+    //        // now, write all odds up to 11
+    //        MAX_ODD = 11;
+    //        writeOdds();
+    //    }
+
+
+    //solution
+
+
+    //public static final int MAX_ODD = 21;
+    //
+    //
+    //    public static void writeOdds11() {
+    //        // print each odd number
+    //        int count;
+    //        for (count = 1; count <= ((MAX_ODD-10) - 2); count = count + 2) {
+    //            System.out.print(count + " ");
+    //            
+    //        }
+    //
+    //        // print the last odd number
+    //        System.out.print(count);
+    //    }
+    //
+    //    public static void writeOdds21() {
+    //        // print each odd number
+    //        int count;
+    //        for (count = 1; count <= (MAX_ODD - 2); count = count + 2) {
+    //            System.out.print(count + " ");
+    //            ;
+    //        }
+    //
+    //        // print the last odd number
+    //        System.out.print(count + 2);
+    //    }
+    //
+    //    public static void main(String[] args) {
+    //        // write all odds up to 21
+    //        writeOdds21();
+    //
+    //        System.out.println();
+    //        // now, write all odds up to 11
+    //        
+    //        writeOdds11();
+    //    }
+}
